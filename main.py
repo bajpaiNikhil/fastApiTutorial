b52466cb9ab98f6ff19af66f4456defe9d9cbac0
NikhilBajpai <nikhil.cse16@gmail.com>
2022-02-26 16:41:50 +0530

    day 1 adding home/account/packages

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
+import fastapi
+import fastapi_chameleon
+import uvicorn
+from views import home
+from views import account
+from views import packages
 
-print("Iamstupid")
+app = fastapi.FastAPI()
+
+
+def main():
+    configure()
+    uvicorn.run(app)
+
+
+def configure():
+    configure_templates()
+    configure_routes()
+
+
+def configure_templates():
+    fastapi_chameleon.global_init('templates')
+
+
+def configure_routes():
+    app.include_router(home.router)
+    app.include_router(account.router)
+    app.include_router(packages.router)
+
+
+if __name__ == '__main__':
+    main()
+else:
+    configure()
