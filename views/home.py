abba5c1733833523a8f65f95efb4c9e05bd3fc8e
NikhilBajpai <nikhil.cse16@gmail.com>
2022-02-27 10:50:06 +0530

    day 2 adding custom look and feel i.e adding html/css

--- views/home.py
+++ views/home.py
@@ -6,12 +6,20 @@ router = fastapi.APIRouter()
 
 @router.get('/')
 @template()
-def index(user: str = "choti"):
+def index():
     return {
-        "user_name": user
+        "package_count" : 254_030 ,
+        "release_count": 254_030,
+        "user_count": 254_030,
+        "packages":[
+            {'id': 'fastapi', 'summary': "A great web framework"},
+            {'id': 'uvicorn', 'summary': "Your favorite ASGI server"},
+            {'id': 'httpx', 'summary': "Requests for an async world"},
+        ]
     }
 
 
 @router.get('/about')
+@template()
 def about():
     return {}
